8faa3fd303b24b3849ce3c9790fc8f9c66d95625
Alex Ozer <alexozer@users.noreply.github.com>
2018-11-06 03:00:00 -0500

    Start attempt at simpler, less strict approach

--- dapper.cpp
+++ dapper.cpp
@@ -63,60 +63,52 @@ FILE *capture(const char *cmd[]) {
   return fdopen(pipe_fds[0], "r");
 }
 
-Document parse_bspc_json(const char *cmd[]) {
-  FILE *stream = capture(cmd);
-  FileReadStream is(stream, buffer, BUF_SIZE);
+Document json_from_file(FILE *file) {
+  FileReadStream is(file, buffer, BUF_SIZE);
 
   Document d;
   d.ParseStream(is);
 
-  fclose(stream);
+  fclose(file);
   return d;
 }
 
-// window id * window class
-typedef std::pair<int, std::string> Window;
+Document parse_bspc_json(const char *cmd[]) {
+  return json_from_file(capture(cmd));
+}
 
-enum class SplitDirection { left, right, none };
+Document parse_config() {
+  std::string target_path =
+      std::string(getenv("HOME")) + "/.config/dapper/config.json";
+  FILE *file = fopen(target_path.c_str(), "r");
+  if (!file) {
+    err("Could not read config file at: " + target_path);
+  }
 
-struct Tag {
-  std::string origin_desk;
-  bool in_split;
-  std::vector<Window> windows;
-  SplitDirection direction;
+  return json_from_file(file);
+}
+
+struct App {
+  std::vector<std::string> commands;
+  std::vector<std::string> classes;
 };
+typedef std::shared_ptr<App> AppPtr;
 
-typedef std::shared_ptr<Tag> TagPtr;
+struct Config {
+  std::unordered_map<std::string, AppPtr> apps;
+  std::string launcher;
+};
 
 class Dapper {
 private:
-  int m_next_desk;
-  std::vector<std::string> m_free_desks;
-  std::string m_split_desk;
-
-  std::vector<TagPtr> m_view_tags;
-  std::unordered_map<std::string, TagPtr> m_class_tags;
-
-  int m_next_vapp_id;
-  std::unordered_map<int, TagPtr> m_vapp_tags;
-  std::priority_queue<int, std::vector<int>, std::greater<int>> m_free_vapp_ids;
-
   std::string m_shell;
+  Config m_config;
+
+  std::unordered_map<int, std::string> m_window_apps;
 
   void spawn_shell(const std::string &shell_cmd) {
     const char *cmd[] = {m_shell.c_str(), "-c", shell_cmd.c_str(), nullptr};
     spawn(cmd, false);
-  }
-
-  void get_desk_windows(const Value &desk, std::vector<Window> &windows) {
-    const auto &root = desk["root"];
-    if (root["firstChild"].IsNull()) {
-      windows.emplace_back(root["id"].GetInt(),
-                           root["client"]["className"].GetString());
-    } else {
-      get_desk_windows(root["firstChild"], windows);
-      get_desk_windows(root["secondChild"], windows);
-    }
   }
 
   void make_desk(const std::string &name) {
@@ -130,193 +122,37 @@ private:
     spawn(cmd, true);
   }
 
-  void move_window(const Window &window, const std::string &desk_name) {
-    const char *cmd[] = {"bspc",
-                         "node",
-                         std::to_string(window.first).c_str(),
-                         "--to-desktop",
-                         desk_name.c_str(),
-                         nullptr};
-    spawn(cmd, true);
-  }
-
-  void evacuate_split_area() {
-    // Restore all windows from the split desktop
-    if (m_view_tags.size() > 1) {
-      for (auto &tag : m_view_tags) {
-        tag->in_split = false;
-        for (auto &win : tag->windows) {
-          move_window(win, tag->origin_desk);
-        }
-      }
-    }
-    m_view_tags.clear();
-  }
-
-  std::string new_desk_name() {
-    if (m_free_desks.size() > 0) {
-      std::string name = m_free_desks.back();
-      m_free_desks.pop_back();
-      return name;
-    }
-    return std::to_string(m_next_desk++);
-  }
-
-  int new_vapp_id() {
-    if (m_free_vapp_ids.size() > 0) {
-      int id = m_free_vapp_ids.top;
-      m_free_vapp_ids.pop();
-      return id;
-    }
-    return m_next_vapp_id++;
-  }
-
-  void show_tag(TagPtr tag) {
-    evacuate_split_area();
-    focus_desk(tag->origin_desk);
-    m_view_tags.push_back(tag);
-  }
-
-  void show_app(const std::vector<std::string> &classes,
-                const std::string &launch_cmd) {
-    // Check if a tag is already associated with any of theses classes
-    // (if so, assume the tag is already associated with all of them)
-    bool tag_exists = false;
-    for (auto &cls : classes) {
-
-      auto it = m_class_tags.find(cls);
-      if (it != m_class_tags.cend()) {
-        tag_exists = true;
-        auto &tag = it->second;
-
-        // If there's no windows there, launch one
-        if (tag->windows.size() == 0) {
-          spawn_shell(launch_cmd);
-          // Only handle the window once its creation event occurs
-        } else {
-          // Focus the tag
-          show_tag(tag);
-        }
-
-        break; // Assume the tag is associated with all provided classes
-      }
-    }
-
-    if (!tag_exists) {
-      // Create a tag for it
-      auto tag = std::make_shared<Tag>();
-      tag->direction = SplitDirection::none;
-      tag->in_split = false;
-      tag->origin_desk = new_desk_name();
-      make_desk(tag->origin_desk);
-
-      // Associate the tag with each window class
-      for (auto &cls : classes) {
-        m_class_tags[cls] = tag;
-      }
-
-      // Move all matching windows to new desktop
-      bool existing_windows = false;
-      for (auto &vapp_tag : m_vapp_tags) {
-        // Record windows that don't need to be removed from tag
-        std::vector<Window> keep_windows;
-
-        auto &windows = vapp_tag.second->windows;
-
-        for (auto win_it = windows.begin(); win_it != windows.end();) {
-          for (auto &cls : classes) {
-            if (win_it->second == cls) {
-              existing_windows = true;
-              move_window(*win_it, tag->origin_desk);
-              windows.erase(win_it);
-            } else {
-              ++win_it;
-            }
-          }
-        }
-
-        vapp_tag.second->windows = keep_windows;
-      }
-
-      if (existing_windows) {
-        // Clean up any now-empty vapps
-        for (auto it = m_vapp_tags.begin(); it != m_vapp_tags.end();) {
-          if (it->second->windows.size() == 0) {
-            m_vapp_tags.erase(it);
-            m_free_vapp_ids.push(it->first);
-          } else {
-            ++it;
-          }
-        }
-      } else {
-        // Spawn program for this tag, but don't focus desktop yet
-        spawn_shell(launch_cmd);
-      }
-    }
-  }
-
-  void show_vapp(int vapp_idx) {
-    auto it = m_vapp_tags.find(vapp_idx);
-    if (it == m_vapp_tags.end())
-      return;
-
-    show_tag(it->second);
-  }
-
 public:
-  Dapper() : m_next_desk{0} {
-    // Determine a suitable shellfor spawning applications
+  Dapper() {
+    // Determine an appropriate shell
     m_shell = getenv("SHELL");
     if (m_shell.size() == 0) {
       m_shell = "sh";
     }
 
-    const char *state_cmd[] = {"bspc", "query", "-m", "-T", nullptr};
-    auto json = parse_bspc_json(state_cmd);
+    // Read config
+    auto config = parse_config();
 
-    // Find a number we can use to start allocating desktops at
-    for (const auto &desk : json["desktops"].GetArray()) {
-      int n;
-      try {
-        n = std::stoi(desk["name"].GetString());
-      } catch (std::invalid_argument e) {
-        continue;
+    for (auto &entry : config.GetObject()) {
+      std::vector<std::string> commands;
+      for (auto &cmd : entry.value["commands"].GetArray()) {
+        commands.push_back(cmd.GetString());
       }
 
-      if (n >= m_next_desk) {
-        m_next_desk = n + 1;
+      std::vector<std::string> classes;
+      for (auto &cmd : entry.value["commands"].GetArray()) {
+        classes.push_back(cmd.GetString());
       }
+
+      auto app = std::make_shared<App>(commands, classes);
+      m_config.apps[entry.name.GetString()] = app;
     }
 
-    // Create a desktop for showing split views
-    m_split_desk = m_next_desk++;
-    make_desk(m_split_desk);
+    m_config.launcher = config["launcher"].GetString();
 
-    // Get all window ids and tags
-    std::vector<Window> windows;
-    for (const auto &desk : json["desktops"].GetArray()) {
-      get_desk_windows(desk, windows);
-    }
-
-    // Treat all windows as virtual apps for now, assign them tags
-    for (auto &window : windows) {
-      auto tag = std::make_shared<Tag>();
-      tag->origin_desk = m_next_desk++;
-      tag->windows.push_back(window);
-      tag->direction = SplitDirection::none;
-      tag->in_split = false;
-
-      make_desk(tag->origin_desk);
-      move_window(window, tag->origin_desk);
-
-      m_vapp_tags[new_vapp_id()] = tag;
-    }
-
-    // Focus a vapp tag if it exists, otherwose just focus the split desk
-    if (m_vapp_tags.size() > 0) {
-      focus_desk(m_vapp_tags[0]->origin_desk);
-    } else {
-      focus_desk(m_split_desk);
+    // Create desktops for all the apps
+    for (auto &app : m_config.apps) {
+      make_desk(app.first);
     }
   }
 
